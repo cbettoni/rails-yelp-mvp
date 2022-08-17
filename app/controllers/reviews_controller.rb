60820c0ef7f1138caae14b671e1de276d12a60ba
christelle bettoni <christelle.bettoni@gmail.com>
2022-08-17 16:11:16 +0200

    Yelp MVP

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,37 @@
+class ReviewsController < ApplicationController
+  # We need to find the restaurant associated with the review
+  before_action :set_restaurant, only: %i[new create]
+
+  def index
+    @reviews = Review.all
+  end
+
+  def show
+    @review = Review.find(params[:id])
+  end
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:rating, :content)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+puts 'Creating restaurants...'
+chez_martin = {name: 'Chez Martin', address: '1 avenue de la baguette 94000 Creteil', phone_number: '0149754251', category: 'french'}
+pizza_east =  {name: 'Pizza East', address: '56A Shoreditch High St, London E1 6PQ', phone_number: '0149754215', category: 'belgian'}
+musashi_sushi = {name: 'Musashi Sushi', address: '66 rue Samourai 94000 Creteil', phone_number: '0149757120', category: 'japanese'}
+pepe_pizza =  {name: 'Pepe Pizza', address: '50 rue de la mozzarella 94320 Bonneuil', phone_number: '0248759632', category: 'italian'}
+baokiller = {name: 'BaoKiller', address: '70 avenue du soleil 94000 Creteil', phone_number: '0489653214', category: 'chinese'}
+[chez_martin, pizza_east, pepe_pizza, baokiller, musashi_sushi].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name} #{restaurant.address} #{restaurant.phone_number} #{restaurant.category}"
+end
+puts 'Finished!'
